e794f39effef98be3f6ef7b9e0b8dea13372018c
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-07-19 13:26:13 +0200

    fix: prop-types

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -48,4 +48,6 @@ Statistics.propTypes = {
   good: PropTypes.number.isRequired,
   neutral: PropTypes.number.isRequired,
   bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.string.isRequired,
 };
